ea2475d173e7ccc2fd71b3d2b668fdb525afabd1
Nwabueze John <143359560+Johnnwabueze01@users.noreply.github.com>
2024-10-27 19:08:20 -0700

    Add files via upload

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>
+        Upgrade your wardrobe with our Classic Cotton Crew Neck T-Shirt crafted from ultra-soft,
+        breathable cotton for all-day comfort and style. This versatile staple features a relaxed fit,
+        reinforced stitching, and is available in timeless colors. Perfect for any occasion,
+        it’s designed to keep its shape and softness wash after wash. Grab yours today and experience
+        the essential tee every closet needs!</p>
+        <p>
+        Step up your style with our Classic Cotton Crew Neck T-Shirt, crafted from premium, breathable
+         cotton for lasting softness and comfort. With a relaxed fit and reinforced stitching, this tee
+          combines durability with effortless style, making it perfect for any occasion from casual
+         outings to layering under jackets. Available in versatile colors like Black, White, Navy, and Gray,
+        it’s designed to retain its shape and feel wash after wash.
+        Choose from sizes XS to XXL and enjoy free shipping on orders over $50, plus a 30-day return policy.
+         Make this essential, easy-to-style tee your go-to for both comfort and timeless appeal.</p>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
